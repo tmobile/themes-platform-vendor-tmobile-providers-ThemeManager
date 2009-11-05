ede7d66435a5e72f543b8ac89327940e5df098d4
Josh Guilfoyle <Josh.Guilfoyle@T-Mobile.com>
2009-11-04 22:33:31 -0800

    Removing unused code.
    CR: Ed Carrigan

--- src/com/tmobile/thememanager/utils/DatabaseUtilities.java
+++ src/com/tmobile/thememanager/utils/DatabaseUtilities.java
@@ -1,21 +1,8 @@
 package com.tmobile.thememanager.utils;
 
-import android.net.Uri;
 import android.text.TextUtils;
 
-import java.util.List;
-
 public class DatabaseUtilities {
-    public static String[] getLastTwoPathSegments(Uri uri) {
-        List<String> segments = uri.getPathSegments();
-        int n = segments.size();
-        if (n < 2) {
-            throw new IllegalArgumentException(uri.toString() + ": " +
-                    "Uri must have at least 2 path segments.");
-        }
-        return new String[] { segments.get(n - 2), segments.get(n - 1) };
-    }
-
     public static String appendSelection(String selection, String extra) {
         if (TextUtils.isEmpty(extra)) {
             throw new IllegalArgumentException("extra must be non-null and non-empty");
@@ -25,20 +12,20 @@ public class DatabaseUtilities {
         }
         return "(" + selection + ") AND " + extra;
     }
-    
+
     public static String[] appendSelectionArgs(String[] selectionArgs, String... extra) {
         if (extra == null || extra.length == 0) {
             throw new IllegalArgumentException("extra must be non-null and non-empty");
         }
-        
+
         if (selectionArgs == null || selectionArgs.length == 0) {
             return extra;
         }
-        
+
         String[] newArgs = new String[selectionArgs.length + extra.length];
         System.arraycopy(selectionArgs, 0, newArgs, 0, selectionArgs.length);
         System.arraycopy(extra, 0, newArgs, selectionArgs.length, extra.length);
-        
+
         return newArgs;
     }
 }
--- src/com/tmobile/thememanager/utils/IOUtilities.java
+++ src/com/tmobile/thememanager/utils/IOUtilities.java
@@ -1,8 +1,5 @@
 package com.tmobile.thememanager.utils;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 import java.io.Closeable;
 import java.io.IOException;
 
@@ -11,32 +8,5 @@ public class IOUtilities {
         try {
             stream.close();
         } catch (IOException e) {}
-    }
-
-    /**
-     * Efficient alternative to {@link Parcel#writeParcelable} which avoids the
-     * reflection overhead.
-     */
-    public static void writeParcelableToParcel(Parcel dest, Parcelable parcelable, int flags) {
-        if (parcelable == null) {
-            dest.writeInt(0);
-        } else {
-            dest.writeInt(1);
-            parcelable.writeToParcel(dest, flags);
-        }
-    }
-    
-    /**
-     * Efficient alternative to {@link Parcel#readParcelable} which avoids the
-     * reflection overhead.
-     */
-    public static <T extends Parcelable> T readParcelableFromParcel(Parcel source,
-            Parcelable.Creator<T> creator) {
-        int nullFlag = source.readInt();
-        if (nullFlag == 0) {
-            return null;
-        } else {
-            return creator.createFromParcel(source);
-        }
     }
 }
